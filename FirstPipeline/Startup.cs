0c88530c14e7edb60aece756a5dbd57d67e29a61
Luis Llerena <luisllerena55@hotmail.com>
2019-08-02 16:52:01 -0500

    remove https reirection

--- FirstPipeline/Startup.cs
+++ FirstPipeline/Startup.cs
@@ -40,7 +40,7 @@ namespace FirstPipeline
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
